af8f01f3d0b885bb30f0284a169f28f43ef9a7bc
phuriungung <puriung08@gmail.com>
2023-08-31 18:13:50 +0700

    add UIViewRepresentable but doesn't work yet

--- writablenocrash/ContentView.swift
+++ writablenocrash/ContentView.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import UIKit
+
+struct RepresentView: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        var initialView = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        
+        initialView.backgroundColor  = UIColor(red: 250, green: 1, blue: 1, alpha: 0.5)
+        
+        return initialView
+    }
+    
+    
+    typealias UIViewType = UIView
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        var initialView = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        
+        initialView.backgroundColor  = UIColor(red: 250, green: 1, blue: 1, alpha: 1)
+        
+    }
+}
+
 
 struct ContentView: View {
     var body: some View {
         VStack {
+            RepresentView()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
