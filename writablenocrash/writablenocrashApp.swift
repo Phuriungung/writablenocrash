2c18d598cc75a10a66b33a7e3b85921fb6e2a81f
phuriungung <puriung08@gmail.com>
2023-08-31 17:42:49 +0700

    Initial Commit

---
+++ writablenocrash/writablenocrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  writablenocrashApp.swift
+//  writablenocrash
+//
+//  Created by minto on 31/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct writablenocrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
