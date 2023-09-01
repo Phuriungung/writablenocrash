90dca5db542c568313971610f3eb23b0eedad177
phuriungung <puriung08@gmail.com>
2023-09-01 23:04:55 +0700

    point 01234 all fix, remains the performance reload view everytime

--- writablenocrash/ContentView.swift
+++ writablenocrash/ContentView.swift
@@ -8,61 +8,61 @@
 import SwiftUI
 import UIKit
 
-class cachedDraw: UIView {
-    var path = UIBezierPath()
-    var ctr = 0
-    var pts: [CGPoint]
-    
-    init(path: UIBezierPath = UIBezierPath(), ctr: Int = 0, pts: [CGPoint]) {
-        self.path = path
-        self.ctr = ctr
-        self.pts = pts
-        super.init(frame: CGRect.zero)
-    }
-    
-    
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    
-    
-    
-    override func draw(_ rect: CGRect) {
-        path.lineWidth = 10.0 // Set the line width as needed
-        path.lineCapStyle = .round
-        UIColor.yellow.setStroke()
-        path.stroke()
-        
-        setNeedsDisplay()
-        ctr = 0
-        
-//        path.move(to: pts[0])
-//        path.addLine(to: pts[0])
-        path.stroke()
-        setNeedsDisplay()
-        
-        
-        
-        path.stroke()
-        
-        ctr += 1
-        if pts.count == 5 {
-            print(pts)
-            pts[3] = CGPoint(x: (pts[2].x + pts[4].x) / 2.0, y: (pts[2].y + pts[4].y) / 2.0)
-            path.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
-            setNeedsDisplay()
-            
-            pts[0] = pts[3]
-            pts[1] = pts[4]
-            ctr = 1
-            path.stroke()
-            setNeedsDisplay()
-        }
-    }
-    
-}
+//class cachedDraw: UIView {
+//    var path = UIBezierPath()
+//    var ctr = 0
+//    var pts: [CGPoint]
+//
+//    init(path: UIBezierPath = UIBezierPath(), ctr: Int = 0, pts: [CGPoint]) {
+//        self.path = path
+//        self.ctr = ctr
+//        self.pts = pts
+//        super.init(frame: CGRect.zero)
+//    }
+//
+//
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//
+//
+//
+//    override func draw(_ rect: CGRect) {
+//        path.lineWidth = 10.0 // Set the line width as needed
+//        path.lineCapStyle = .round
+//        UIColor.yellow.setStroke()
+//        path.stroke()
+//
+//        setNeedsDisplay()
+//        ctr = 0
+//
+////        path.move(to: pts[0])
+////        path.addLine(to: pts[0])
+//        path.stroke()
+//        setNeedsDisplay()
+//
+//
+//
+//        path.stroke()
+//
+//        ctr += 1
+//        if pts.count == 5 {
+//            print(pts)
+//            pts[3] = CGPoint(x: (pts[2].x + pts[4].x) / 2.0, y: (pts[2].y + pts[4].y) / 2.0)
+//            path.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
+//            setNeedsDisplay()
+//
+//            pts[0] = pts[3]
+//            pts[1] = pts[4]
+//            ctr = 1
+//            path.stroke()
+//            setNeedsDisplay()
+//        }
+//    }
+//
+//}
 
 
 
@@ -71,23 +71,43 @@ class cachedDraw: UIView {
 
 class SmoothedDraw: UIView {
     var path = UIBezierPath()
+    var path2 = UIBezierPath()
     var ctr = 0
     var pts = [CGPoint](repeating: .zero, count: 5)
     var savedpts: [CGPoint] = []
     
+    
     var whitePath = UIBezierPath()
     
-    var cachedDrawView = cachedDraw(pts: [])
+    //    var cachedDrawView = cachedDraw(pts: [])
     
     override func draw(_ rect: CGRect) {
         //        path = UIBezierPath(roundedRect: rect, cornerRadius:10)
         path.lineWidth = 10.0 // Set the line width as needed
         path.lineCapStyle = .round
         
-        UIColor.red.setStroke()
-       
+        UIColor.blue.setStroke()
         
+        //        path.move(to: CGPoint(x: 500, y: 500))
+        //        path.addLine(to: CGPoint(x: 500, y: 900))
+        //        path.stroke()
+        
+        
+        path2.lineWidth = 10.0
+        path2.lineCapStyle = .round
+        
+        //        UIColor.blue.setStroke()
+        //        path2.stroke()
+        //        setNeedsDisplay()
+        
+        
+        //        UIColor.red.setStroke()
+        //        path.move(to: CGPoint(x: 900, y: 500))
+        //        path.addLine(to: CGPoint(x: 500, y: 900))
         path.stroke()
+        UIColor.blue.setStroke()
+        path2.stroke()
+        setNeedsDisplay()
         
     }
     
@@ -117,7 +137,7 @@ class SmoothedDraw: UIView {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         path.stroke()
-        //        print(touches)
+        //                print(touches)
         if let touch = touches.first {
             let p = touch.location(in: self)
             ctr += 1
@@ -129,36 +149,110 @@ class SmoothedDraw: UIView {
             //                setNeedsDisplay()
             //            }
             //
-            //            if ctr == 3 {
-            //                path.move(to: pts[0])
-            //                path.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
-            //                path.stroke()
-            //                setNeedsDisplay()
-            //            }
+            
+            if ctr == 1 {
+                path.move(to: pts[0])
+                path.addLine(to: pts[1])
+                path.stroke()
+                setNeedsDisplay()
+                print("1")
+            }
+            
+            if ctr == 2 {
+                path.removeAllPoints()
+                path.move(to: pts[0])
+                path.addQuadCurve(to: pts[2], controlPoint: pts[1])
+                path.stroke()
+                setNeedsDisplay()
+                print("2")
+            }
+            
+            
+            if ctr == 3 {
+                path.removeAllPoints()
+                path.move(to: pts[0])
+                path.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
+                path.stroke()
+                setNeedsDisplay()
+                print("3")
+            }
             
             if ctr == 4 {
+                path.removeAllPoints()
                 pts[3] = CGPoint(x: (pts[2].x + pts[4].x) / 2.0, y: (pts[2].y + pts[4].y) / 2.0)
-                path.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
+                path2.move(to: pts[0])
+                path2.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
+//                path2.addLine(to: CGPoint(x: 500, y: 900))
+                path2.stroke()
                 setNeedsDisplay()
+                print("4")
+                
+                
                 
                 for i in 0...4 {
                     savedpts.append(pts[i])
                 }
-//                print(savedpts)
-                cachedDrawView.pts = savedpts
+//
+//                path2.move(to: savedpts[0])
+//                path2.addCurve(to: savedpts[3], controlPoint1: savedpts[1] , controlPoint2: savedpts[2])
+//                //                print(savedpts)
+//
+//                path2.addLine(to: CGPoint(x: 500, y: 1000))
+//                path2.addLine(to: savedpts[1])
+//                path2.addLine(to: savedpts[2])
+//
+//                for i in 1...savedpts.count-1 {
+//                    path2.addLine(to: savedpts[i])
+//                }
+//
+//                path2.stroke()
+//                setNeedsDisplay()
                 
                 
                 pts[0] = pts[3]
                 pts[1] = pts[4]
+                print("5")
                 ctr = 1
-                path.stroke()
-                setNeedsDisplay()
+                
             }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if ctr == 0 {
+            path2.move(to: pts[0])
+            path2.addLine(to: pts[0])
+            path2.stroke()
+            setNeedsDisplay()
+        }
+        
+        if ctr == 1 {
+            path2.move(to: pts[0])
+            path2.addLine(to: pts[1])
+            path2.stroke()
+            setNeedsDisplay()
+            print("e1")
+        }
+        
+        if ctr == 2 {
+            path2.move(to: pts[0])
+            path2.addQuadCurve(to: pts[2], controlPoint: pts[1])
+            path2.stroke()
+            setNeedsDisplay()
+            print("e2")
+        }
+        
+        if ctr == 3 {
+            path2.move(to: pts[0])
+            path2.addCurve(to: pts[3], controlPoint1: pts[1], controlPoint2: pts[2])
+            path2.stroke()
+            setNeedsDisplay()
+            print("e3")
+        }
+        
         ctr = 0
+//        path.removeAllPoints()
     }
     
 }
@@ -204,25 +298,12 @@ struct RepresentView: UIViewRepresentable {
         
         let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 2000))
         
-        var smoothDrawInstance = SmoothedDraw()
-        smoothDrawInstance.backgroundColor = UIColor(red: 0, green: 0, blue: 0.5, alpha: 0.5)
+        let smoothDrawInstance = SmoothedDraw()
+        smoothDrawInstance.backgroundColor = UIColor(.white)
         smoothDrawInstance.frame = CGRect(x: 0, y: 0, width: 500, height: 2000)
         
-        var cachedDrawInstance = cachedDraw(pts: smoothDrawInstance.savedpts)
         
-        // Add cachedDrawInstance as a subview of containerView
-        containerView.addSubview(cachedDrawInstance)
-        
-        // Set cachedDrawInstance's frame (you may need to adjust this)
-        cachedDrawInstance.frame = CGRect(x: 0, y: 0, width: 500, height: 2000)
-        
-        // Add smoothDrawInstance as a subview of containerView
-        containerView.addSubview(smoothDrawInstance)
-        
-        // Set cachedDrawInstance's pts property with savedpts
-        cachedDrawInstance.pts = smoothDrawInstance.savedpts
-        
-        return containerView
+        return smoothDrawInstance
         
     }
     
